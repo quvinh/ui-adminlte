a574f512a5d7f0b80658e385e241a86d87de822e
quvinh <vinhhp2620@gmail.com>
2022-03-16 12:30:31 +0700

    add login

--- src/App.js
+++ src/App.js
@@ -1,161 +1,28 @@
-import React, { Component } from 'react';
-import AdminLTE, { Sidebar, Navbar, Content, Row, Col, Box, Button } from 'adminlte-2-react';
-import user1Image from './assets/img/user1-128x128.jpg';
-import user2Image from './assets/img/user2-160x160.jpg';
-import user3Image from './assets/img/user3-128x128.jpg';
-import user4Image from './assets/img/user4-128x128.jpg';
-import moment from 'moment';
+import React from 'react'
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom'
+import { getToken } from './components/utils/Common'
 
-const { Item, Header } = Sidebar;
-const {
-  MessageItem, Entry, NotificationItem, TaskItem,
-} = Navbar;
-const messageMenu = [
-  {
-    text: 'Support Team',
-    subText: 'Why not buy a new awesome theme?',
-    imageUrl: user1Image,
-    when: moment().subtract(5, 'minutes'),
-  },
-  {
-    text: 'AdminLTE Design Team',
-    subText: 'Why not buy a new awesome theme?',
-    imageUrl: user2Image,
-    when: moment().subtract(2, 'hours'),
-  },
-  {
-    text: 'Developers',
-    subText: 'Why not buy a new awesome theme?',
-    imageUrl: user3Image,
-    when: moment().subtract(13, 'hours'),
-  },
-  {
-    text: 'Sales departement',
-    subText: 'Why not buy a new awesome theme?',
-    imageUrl: user4Image,
-    when: moment().subtract(1, 'day'),
-  },
-  {
-    text: 'Reviewers',
-    subText: 'Why not buy a new awesome theme?',
-    imageUrl: user3Image,
-    when: moment().subtract(2, 'days'),
-  },
-]
+const Default = React.lazy(() => import('./views/Default'))
+const Login = React.lazy(() => import('./views/pages/Login'))
+// const Register = React.lazy(() => import('./views/pages/Register'))
+// const Page404 = React.lazy(() => import('./views/pages/Page404'))
 
-const notificationMenu = [
-  {
-    icon: 'fas-users',
-    iconColor: 'aqua',
-    text: '5 new members joined today',
-  },
-  {
-    icon: 'fas-exclamation-triangle',
-    iconColor: 'yellow',
-    text: ' Very long description here that may not fit into the page and may cause design problems',
-  },
-  {
-    icon: 'fas-users',
-    iconColor: 'red',
-    text: '5 new members joined today',
-  },
-  {
-    icon: 'fas-shopping-cart',
-    iconColor: 'green',
-    text: '25 sales made',
-  },
-  {
-    icon: 'fas-user',
-    iconColor: 'red',
-    text: 'You changed your username',
-  },
-]
-
-const taskMenu = [
-  {
-    value: 20,
-    barColor: 'aqua',
-    text: 'Design some buttons',
-  },
-  {
-    value: 40,
-    barColor: 'green',
-    text: 'Create a nice theme',
-  },
-  {
-    value: 60,
-    barColor: 'red',
-    text: 'Some task I need to do',
-  },
-  {
-    value: 80,
-    barColor: 'yellow',
-    text: 'Make beautiful transitions',
-  },
-]
-
-const App = () => {
-
-  const sidebar = [
-    <Item key="hello" text="Hello" to="/hello-world" />
-  ]
-
+//  fallback={loading}
+function App() {
   return (
-    // <AdminLTE title={["Hello", "World"]} titleShort={["He", "we"]} theme="blue" sidebar={sidebar}>
-    //   <HelloWorld path="/hello-world" />
-    // </AdminLTE>
-    <AdminLTE browserTitle="adminlte-2-react-examples" theme="black">
-      <Navbar.Core>
-        <Entry
-          icon="fas-envelope"
-        >
-          {messageMenu.map(p => <MessageItem {...p} />)}
-          {notificationMenu.map(p => <NotificationItem {...p} />)}
-        </Entry>
-        <Entry
-          icon="fas-bell"
-          className="notifications-menu"
-          labelType="warning"
-        >
-          {notificationMenu.map(p => <NotificationItem {...p} />)}
-        </Entry>
-        <Entry
-          icon="far-flag"
-          className="tasks-menu"
-          labelType="danger"
-          headerText="You have #value# tasks"
-          footerText="View all tasks"
-        >
-          {taskMenu.map(p => <TaskItem {...p} />)}
-        </Entry>
-        <Entry
-          icon="fas-power-off"
-        />
-      </Navbar.Core>
-      <Sidebar.Core>
-        <Item icon="fas-pencil-ruler" text="NAM KHANH" to="/" />
-        <Header text="MAIN NAVIGATION" />
-        <Item to="/widgets" text="Bảng tin" icon="fa-th" labels={[{ color: 'green', text: 'new', small: true }]} />
-        <Item text="Danh mục" icon="fa-chart-pie">
-          <Item text="Loại vật tư" to="/chart-js" />
-        </Item>
-        <Item active text="Chức năng" icon="fa-tachometer-alt">
-          <Item text="Nhập kho" to="/dashboard" />
-          <Item text="Xuất kho" to="/dashboardv2" />
-          <Item text="Luân chuyển" to="/dashboardv2" />
-        </Item>
-        <Item text="Quản lý kho" icon="far-copy">
-          <Item text="Kho" to="/top-nav" />
-          <Item text="Giá/kệ" to="/boxed" />
-        </Item>
-        <Item text="Admin" icon="fa-laptop">
-          <Item text="Người dùng" to="/ui-elements/general" />
-          <Item text="Chức năng - Quyền hạn" to="/ui-elements/icons" />
-        </Item>
-        <Item to="/widgets" text="Thông báo" icon="fa-th" labels={[{ color: 'green', text: 'new', small: true }]} />
-      </Sidebar.Core>
-    </AdminLTE>
-  )
+    <>
+      <HashRouter>
+        <React.Suspense fallback={<h1>Loading...</h1>}>
+          <Switch>
+            <Route exact path="/login" name="Login Page" render={(props) => <Login {...props} />} />
+            {/* <Route exact path="/register" name="Register Page" render={(props) => <Register {...props} />} />
+            <Route exact path="/404" name="Page 404" render={(props) => <Page404 {...props} />} /> */}
+            <Route path="/" name="Home" render={(props) => getToken() ? <Default {...props} /> : <Login {...props} />} />
+          </Switch>
+        </React.Suspense>
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { postData } from '../../components/utils/Api'
+import { setUserSession } from '../../components/utils/Common'
+import {
+  Content, Row, Col, Box, Button, Inputs, Divider
+} from 'adminlte-2-react';
 
+const {
+  Text, Checkbox, Radio, Select
+} = Inputs;
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const history = useHistory()
+
+  const handleLogin = () => {
+    const data = {
+      username: username,
+      password: password
+    }
+    console.log(data)
+    Promise.all([postData("http://127.0.0.1:8000/api/auth/login", data)])
+      .then(function (res) {
+        console.log(res[0].data.warehouse_id)
+        setUserSession(res[0].data.access_token, res[0].data.user.id, res[0].data.user.roles[0].name, res[0].data.role, res[0].data.warehouse_id)
+        console.log("login")
+        history.push("/dashboard")
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }
+
   return (
-    <div>Login</div>
+    <div className="hold-transition login-page">
+      <div className="login-box">
+        <div className="login-logo">
+          <a><b>NAM</b>KHÁNH</a>
+        </div>
+        <div className="card">
+          <div className="card-body login-card-body">
+            <p className="login-box-msg">Sign in to start your session</p>
+            <form id="quickForm">
+              <div className="card-body">
+                <div className="form-group">
+                  <label>Tên đăng nhập</label>
+                  <input type="text" name="username" value={username} className="form-control" placeholder="Tên đăng nhập" onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div className="form-group">
+                  <label>Mật khẩu</label>
+                  <input type="password" name="password" value={password} className="form-control" placeholder="Mật khẩu" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+              </div>
+              <div className="social-auth-links text-center mb-3">
+                <button type="button" className="btn btn-primary" onClick={() => handleLogin()}>Đăng nhập</button>
+              </div>
+              {/* <div className="card-footer">
+                <button type="submit" className="btn btn-primary">Đăng nhập</button>
+              </div> */}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
